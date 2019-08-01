407e8640373136f165a295cca87995f09de24ef9
Bernardo Castilho <bernardo-castilho@hotmail.com>
2019-08-01 10:30:37 -0300

    Delete CellRangeEventArgs.cs

--- Grid/CellRangeEventArgs.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WJ
-{
-    public class CellRangeEventArgs : CancelEventArgs
-    {
-        public CellRange Range { get; set; }
-        public int Row { get => Range.Row; }
-        public int Col { get => Range.Col; }
-    }
-}
